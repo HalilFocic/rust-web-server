55964c4c83d78a9fb91743e309b32211550f538e
Halil Focic <halil.focic@zendev.se>
2024-05-06 20:15:17 +0200

    added basic get handle functionality to read from local file

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
 };
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -19,20 +17,54 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let mut buf_reader = BufReader::new(&mut stream);
 
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-    };
-    let contents = fs::read_to_string(filename).unwrap();
+    // read first line then iterate over rest of the lines in buf_reader
+    let mut first_line = String::new();
+    buf_reader.read_line(&mut first_line).unwrap();
+    if first_line.starts_with("GET") {
+        return handle_get_request(stream, first_line);
+    }
+    for line in buf_reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
+    let contents = fs::read_to_string("hello.html").unwrap();
     let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("HTTP/1.1 200 OK \r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+fn handle_get_request(mut stream: TcpStream, first_line: String) {
+    let resource = first_line.split_whitespace().collect::<Vec<&str>>()[1];
+    let file_content = read_from_rile(&resource[..]);
+    match file_content {
+        Ok(content) => {
+            let length = content.len();
+            let response = format!(
+                "HTTP/1.1 200 OK \r\nContent-Length: {length}\r\n\r\n{content}",
+                length = length,
+                content = content
+            );
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+        Err(e) => {
+            let response = format!("HTTP/1.1 404 NOT FOUND \r\n\r\n{}", e);
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+   }
+    /* stream
+        .write_all(
+            format!(
+                "HTTP/1.1 200 OK \r\n\r\n Hello from backend {}",
+                file_content
+            )
+            .as_bytes(),
+        )
+        .unwrap();*/
+}
+fn read_from_rile(file_name: &str) -> Result<String, std::io::Error> {
+    let file_name = format!("./public/{}.txt", file_name);
+    println!("file_name: {}", file_name);
+    fs::read_to_string(file_name)
 }
